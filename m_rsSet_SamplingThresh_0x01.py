cf679f1fe937dd17295c2582dc6f064a9797072d
mateovallejo <vallejo.mateo1@gmail.com>
2024-08-24 18:20:57 -0300

    Redshift Scripts

---
+++ m_rsSet_SamplingThresh_0x01.py
@@ -0,0 +1,41 @@
+import c4d
+import c4d.documents
+import redshift
+
+def FindVideoPost(renderData, pluginId):
+    vp = renderData.GetFirstVideoPost()
+    while vp is not None:
+        if vp.IsInstanceOf(pluginId):
+            return vp
+        vp = vp.GetNext()
+    return None
+
+def FindAddVideoPost(renderData, vpPluginID):
+    vp = FindVideoPost(renderData, vpPluginID)
+    if vp is None:
+        vp = c4d.documents.BaseVideoPost(vpPluginID)
+        if vp is not None:
+            renderData.InsertVideoPost(vp)
+    return vp
+
+def main():
+    doc = c4d.documents.GetActiveDocument()
+    renderdata = doc.GetActiveRenderData()
+
+    vprs = FindAddVideoPost(renderdata, redshift.VPrsrenderer)
+    if vprs is None:
+        return
+
+    # Switch renderer
+    renderdata[c4d.RDATA_RENDERENGINE] = redshift.VPrsrenderer
+
+    # Set some parameters
+    vprs[c4d.REDSHIFT_RENDERER_UNIFIED_ADAPTIVE_ERROR_THRESHOLD] = 0.01
+
+
+    # Refresh UI
+    c4d.EventAdd()
+
+
+if __name__ == '__main__':
+    main()
